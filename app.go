fe043eda0794b83cd3786839baeea07540204c9d
hubbusysyuhada <hubbusysyuhada@gmail.com>
2021-10-03 21:21:54 +0700

    new version

--- app.go
+++ app.go
@@ -5,7 +5,7 @@ import "fmt"
 func SayHi() {
 	defer end()
 	fmt.Println("hello guys, this is method from say hi modules")
-	fmt.Println("new version of 1.0.1")
+	fmt.Println("new version of 1.0.2")
 }
 
 func init() {
